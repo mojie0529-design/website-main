12ff39afec6c73f636eb4f9140f036cb94241d55
jiemo <mojie0529@gmail.com>
2025-11-09 13:46:01 +0800

    清理 main 分支，代码移至 qiao 分支

--- src/constant/host.ts
+++
@@ -1,2 +0,0 @@
-export const baseUrl = process.env.PAY_BASE_URL as string;
-export const host = process.env.PAY_HOST as string;
--- src/hooks/path.ts
+++
@@ -1,5 +0,0 @@
-import useUrlState from '@ahooksjs/use-url-state';
-export const useQuery = <T extends Record<string, any>>() => {
-    const [state] = useUrlState<T>();
-    return state;
-};
